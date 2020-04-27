21ad1048ebbf05ae22a6e579a04ab9a39079ddb9
Monseg <mons@mons.ws>
2020-04-27 15:49:23 +0300

    Update and rename temp_mmt.py to t_monitor.py

---
+++ t_monitor.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+FAN_ON_MODE = 65  # (In celsius) The upper temperature threshold at which cooling is turned on.
+FAN_OFF_MODE = 55  # (In celsius) Lower temperature threshold at which cooling is turned off
+SLEEP_INTERVAL = 5  # (In seconds) Temperature Check Interval
+SLEEP_INTERVAL_FAN = 5  # (In seconds) Temperature Check Interval
+GPIO_PIN = 17  # GPIO pin used for cooling control
+
+def measure_temp():
+       temp = os.popen("vcgencmd measure_temp").readline()
+       #show =  float("%.2f" % temp)
+       temp.replace("temp=","")
+       return(temp.replace("'C",""))
+		
+
+while True:
+       #print(measure_temp())
+       fan = OutputDevice(GPIO_PIN)
+       print(round(measure_temp()))
+       if temp > FAN_ON_MODE:
+                fan.on()
+                time.sleep(SLEEP_INTERVAL_FAN)
+       elif fan.value and temp < FAN_OFF_MODE:
+                fan.off()
+       time.sleep(SLEEP_INTERVAL)

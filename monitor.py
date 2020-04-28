264684db8c9b1b10e722b23d1d23873a1740b1b2
Monseg <mons@mons.ws>
2020-04-28 13:33:56 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,7 +2,7 @@ import os
 import time
 import RPi.GPIO as GPIO
 
-FAN_ON_MODE = 42  # (In celsius) The upper temperature threshold at which cooling is turned on.
+FAN_ON_MODE = 55  # (In celsius) The upper temperature threshold at which cooling is turned on.
 FAN_OFF_MODE = 40  # (In celsius) Lower temperature threshold at which cooling is turned off
 SLEEP_INTERVAL = 2  # (In seconds) Temperature Check Interval
 SLEEP_INTERVAL_FAN = 5  # (In seconds) Temperature Check Interval

087ef85a8908296b7c0befdad802be25e64031d5
Monseg <mons@mons.ws>
2020-04-28 10:28:05 +0300

    Update t_monitor.py

--- t_monitor.py
+++ t_monitor.py
@@ -1,26 +1,29 @@
 import os
 import time
+import RPi.GPIO as GPIO
 
 FAN_ON_MODE = 65  # (In celsius) The upper temperature threshold at which cooling is turned on.
 FAN_OFF_MODE = 55  # (In celsius) Lower temperature threshold at which cooling is turned off
 SLEEP_INTERVAL = 5  # (In seconds) Temperature Check Interval
 SLEEP_INTERVAL_FAN = 5  # (In seconds) Temperature Check Interval
-GPIO_PIN = 17  # GPIO pin used for cooling control
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
 
 def measure_temp():
        temp = os.popen("vcgencmd measure_temp").readline()
        #show =  float("%.2f" % temp)
        temp.replace("temp=","")
        return(temp.replace("'C",""))
-		
+	#return float(m.group(1))	
 
 while True:
-       #print(measure_temp())
-       fan = OutputDevice(GPIO_PIN)
+       #print(measure_temp()
        print(round(measure_temp()))
        if temp > FAN_ON_MODE:
-                fan.on()
+                GPIO.output(4, True)
                 time.sleep(SLEEP_INTERVAL_FAN)
        elif fan.value and temp < FAN_OFF_MODE:
-                fan.off()
+                GPIO.output(4, False)
        time.sleep(SLEEP_INTERVAL)
